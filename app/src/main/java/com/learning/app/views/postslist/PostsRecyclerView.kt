4cb3cbe72c80eb11eb0cde05ffe245c0aff8ae26
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2020-11-03 02:48:58 -0300

    add divider decorator

--- app/src/main/java/com/learning/app/views/postslist/PostsRecyclerView.kt
+++ app/src/main/java/com/learning/app/views/postslist/PostsRecyclerView.kt
@@ -2,9 +2,11 @@ package com.learning.app.views.postslist
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.learning.domain.model.PostItemDomainModel
+
 
 class PostsRecyclerView(context: Context, attrs: AttributeSet? = null) :
     RecyclerView(context, attrs) {
@@ -13,6 +15,8 @@ class PostsRecyclerView(context: Context, attrs: AttributeSet? = null) :
         setHasFixedSize(true)
         overScrollMode = OVER_SCROLL_NEVER
         layoutManager = LinearLayoutManager(context)
+        val itemDecorator = DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL)
+        addItemDecoration(itemDecorator)
     }
 
     fun setData(posts: List<PostItemDomainModel>) {
